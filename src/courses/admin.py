c7d9dd285d7bf30cab765f826ed52030dfa37655
CodingForEntrepreneurs <hello@teamcfe.com>
2024-09-11 14:14:50 -0600

    20 - Foreign Key Stacked Model Admin

--- src/courses/admin.py
+++ src/courses/admin.py
@@ -2,11 +2,17 @@ from cloudinary import CloudinaryImage
 from django.contrib import admin
 from django.utils.html import format_html
 # Register your models here.
-from .models import Course
+from .models import Course, Lesson
+
+
+class LessonInline(admin.StackedInline):
+    model = Lesson
+    extra = 0
 
 
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
+    inlines = [LessonInline]
     list_display = ['title', 'status', 'access']
     list_filter = ['status', 'access']
     fields = ['title', 'description', 'status', 'image', 'access', 'display_image']
