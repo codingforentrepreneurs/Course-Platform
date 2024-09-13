7082574541a09c7f8e4d3d16665266347328f3eb
CodingForEntrepreneurs <hello@teamcfe.com>
2024-09-13 15:14:23 -0600

    34 - View Logic and Database Lookup Services

---
+++ src/courses/services.py
@@ -0,0 +1,34 @@
+from .models import Course, Lesson, PublishStatus
+
+
+def get_publish_courses():
+    return Course.objects.filter(status=PublishStatus.PUBLISHED)
+
+def get_course_detail(course_id=None):
+    if course_id is None:
+        return None
+    obj = None
+    try:
+        obj = Course.objects.get(
+            status=PublishStatus.PUBLISHED,
+            id=course_id
+        )
+    except:
+        pass
+    return obj
+
+
+def get_lesson_detail(course_id=None, lesson_id=None):
+    if lesson_id is None or course_id is None:
+        return None
+    obj = None
+    try:
+        obj = Lesson.objects.get(
+            course___id=course_id,
+            course___status=PublishStatus.PUBLISHED,
+            status=PublishStatus.PUBLISHED,
+            id=lesson_id
+        )
+    except:
+        pass
+    return obj
--- src/courses/views.py
+++ src/courses/views.py
@@ -1,3 +1,25 @@
+from django.http import Http404
 from django.shortcuts import render
 
-# Create your views here.
+from . import services
+
+def course_list_view(request):
+    queryset = services.get_publish_courses()
+    return render(request, "courses/list.html", {})
+
+
+def course_detail_view(request, course_id=None, *args, **kwarg):
+    course_obj = services.get_course_detail(course_id=course_id)
+    if course_obj is None:
+        raise Http404
+    return render(request, "courses/detail.html", {})
+
+
+def lesson_detail_view(request, course_id=None, lesson_id=None, *args, **kwargs):
+    lesson_obj = services.get_lesson_detail(
+        course_id=course_id,
+        lesson_id=lesson_id
+    )
+    if lesson_obj is None:
+        raise Http404
+    return render(request, "courses/lesson.html", {})
