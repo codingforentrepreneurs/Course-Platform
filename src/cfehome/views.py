a8a8c90f7e960dd7c294de236d54a9040c54a289
CodingForEntrepreneurs <hello@teamcfe.com>
2024-09-17 11:53:21 -0600

    46 - Handing HTML Forms with Django

--- src/cfehome/views.py
+++ src/cfehome/views.py
@@ -7,14 +7,17 @@ EMAIL_ADDRESS = settings.EMAIL_ADDRESS
 def home_view(request, *args, **kwargs):
     template_name = "home.html"
     # request POST data
-    
+    print(request.POST)
     form = EmailForm(request.POST or None)
     context = {
         "form": form,
         "message": ""
     }
     if form.is_valid():
-        form.save()
+        obj = form.save()
+        print(obj)
         context['form'] = EmailForm()
-        context['message'] = f"Succcess! Check your email for verification from {EMAIL_ADDRESS}" 
+        context['message'] = f"Succcess! Check your email for verification from {EMAIL_ADDRESS}"
+    else:
+        print(form.errors) 
     return render(request, template_name, context)
