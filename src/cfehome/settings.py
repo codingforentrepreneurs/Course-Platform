fdf68beabf65d8cfc63feaa6346c7f40b3d67cb1
CodingForEntrepreneurs <hello@teamcfe.com>
2024-09-17 11:26:32 -0600

    44 - Email Verification Models

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -63,6 +63,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "courses",
+    "emails"
 ]
 
 MIDDLEWARE = [
---
+++ src/emails/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+# Create your models here.
+class Email(models.Model):
+    email = models.EmailField(unique=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+
+class EmailVerificationEvent(models.Model):
+    parent = models.ForeignKey(Email, on_delete=models.SET_NULL, null=True)
+    email = models.EmailField() 
+    #  token
+    attempts = models.IntegerField(default=0)
+    last_attempt_at = models.DateTimeField(
+        auto_now=False,
+        auto_now_add=False,
+        blank=True,
+        null=True
+    )
+    expired = models.BooleanField(default=False)
+    expired_at = models.DateTimeField(
+        auto_now=False,
+        auto_now_add=False,
+        blank=True,
+        null=True
+    )
+    timestamp = models.DateTimeField(auto_now_add=True)
