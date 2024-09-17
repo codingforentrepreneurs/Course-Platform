178f9190a458ef5737cd9a12ff6056a6843300d2
CodingForEntrepreneurs <hello@teamcfe.com>
2024-09-17 16:01:24 -0600

    56 - HTMX with POST Data

--- src/emails/views.py
+++ src/emails/views.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.contrib import messages
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
+from django_htmx.http import HttpResponseClientRedirect
 
 from . import services
 
@@ -12,17 +13,21 @@ EMAIL_ADDRESS = settings.EMAIL_ADDRESS
 def email_token_login_view(request):
     if not request.htmx:
         return redirect('/')
+    email_id_in_session = request.session.get('email_id')
     template_name = "emails/hx/form.html"
     form = EmailForm(request.POST or None)
     context = {
         "form": form,
-        "message": ""
+        "message": "",
+        "show_form": not email_id_in_session,
     }
     if form.is_valid():
         email_val = form.cleaned_data.get('email')
         obj = services.start_verification_event(email_val)
         context['form'] = EmailForm()
         context['message'] = f"Succcess! Check your email for verification from {EMAIL_ADDRESS}"
+        # return HttpResponseClientRedirect('/check-your-email')
+        return render(request, template_name, context)
     else:
         print(form.errors) 
     return render(request, template_name, context)
