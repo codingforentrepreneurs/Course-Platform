f5799b26c9c8b82cb6f2d355c94daa6d89321b96
CodingForEntrepreneurs <hello@teamcfe.com>
2024-09-17 13:40:15 -0600

    50 - Send Email from Verification Event

--- src/emails/services.py
+++ src/emails/services.py
@@ -1,8 +1,19 @@
+from django.conf import settings
+from django.core.mail import send_mail
 from .models import Email, EmailVerificationEvent
+
+EMAIL_HOST_USER = settings.EMAIL_HOST_USER
 
 def verify_email(email):
     qs = Email.objects.filter(email=email, active=False)
     return qs.exists()
+
+def get_verification_email_msg(verification_instance, as_html=False):
+    if not isinstance(verification_instance, EmailVerificationEvent):
+        return None
+    if as_html:
+        return f"<h1>{verification_instance.id}</h1>"
+    return f"{verification_instance.id}"
 
 
 def start_verification_event(email):
@@ -11,5 +22,24 @@ def start_verification_event(email):
         parent=email_obj,
         email=email
     )
+    sent = send_verification_email(obj.id)
+    return obj, sent
+
+# celery task -> background task
+def send_verification_email(verify_obj_id,):
+    verify_obj = EmailVerificationEvent.objects.get(id=verify_obj_id)
+    email = verify_obj.email
+    subject = "Verify your email"
+    text_msg = get_verification_email_msg(verify_obj, as_html=False)
+    text_html = get_verification_email_msg(verify_obj, as_html=True)
+    from_user_email_addr = EMAIL_HOST_USER
+    to_user_email = email
     # send an verification email
-    return obj
+    return send_mail(
+        subject,
+        text_msg,
+        from_user_email_addr,
+        [to_user_email],
+        fail_silently=False,
+        html_message=text_html
+    )
